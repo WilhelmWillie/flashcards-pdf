f44e953d236ea95e57bb2b6424c2826622322942
Wilhelm Willie <wilhelmwillie@gmail.com>
2017-08-11 11:32:44 -0400

    Added jsPDF and create a test PDF on page load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import jsPDF from 'jspdf';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor() {
+    let doc = new jsPDF();
+    doc.text("Hello", 20, 20);
+    doc.save("basic.pdf");
+  }
 }

9b3cd47b1545da2cb2bcb4af782a39128e85bc96
Wilhelm Willie <wilhelmwillie@gmail.com>
2017-08-11 13:22:12 -0400

    Flashcard data is passed to component and displays term and definition

--- src/app/flashcard/flashcard.component.ts
+++ src/app/flashcard/flashcard.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-flashcard',
+  selector: 'flashcard',
   templateUrl: './flashcard.component.html',
   styleUrls: ['./flashcard.component.css']
 })
 export class FlashcardComponent implements OnInit {
+  @Input() card;
 
   constructor() { }
 
